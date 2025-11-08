189f87bb94aef150cd90039a419869716840e870
yogeshayer <yogeshayer@my.unt.edu>
2025-11-07 20:15:49 -0600

    fvdf

--- proxy.js
+++ proxy.js
@@ -1,6 +1,6 @@
 import { updateSession } from "@/lib/supabase/middleware"
 
-export async function middleware(request) {
+export async function proxy(request) {
   return await updateSession(request)
 }
 
